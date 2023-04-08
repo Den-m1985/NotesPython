4ee04aafdf5556556e6038a6398b7df80dc30e50
den-m1985 <denismartyanov244@gmail.com>
2023-04-08 19:26:26 +0200

    make controller, print title

--- controller.py
+++ controller.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-import Note
-from JsonNoteReader import JsonNoteReader
+from Note import Note
+from Uniq_id import Uniq_id
 from Operation import Operation
-
+from Correct_body import Correct_body
+from Search_title import Search_title
+from Delete_note import Delete_note
 
 def distribute(n):
 
@@ -11,9 +13,14 @@ def distribute(n):
 
     if n == '1':
         # Создаем заметку
-        note = Note.Note("Заголовок заметки", "Текст заметки")
+        note = Note(file_name)
         # Устанавливаем идентификатор
-        note.set_id(1)
+        id = Uniq_id(file_name).uniq_id()
+        note.set_id(id)
+        title = input('Текст заголовка: ')
+        note.set_title(title)
+        body = input('Текст заметки: ')
+        note.set_body(body)
         # время создания заметки
         note.set_created_at(datetime.now())
         # Сохраняем заметку в формате JSON
@@ -21,30 +28,23 @@ def distribute(n):
 
         operation = Operation(file_name)
         operation.read_to_write_json(note_json)
-        #writer = JsonNoteWriter(file_name)
-        #writer.write(note_json)
-        #new_notes.Note.write_notes(file_name, note_json)
-        #write_json.write_json(note_json, file_name)
-        enter = input('Нажмите Enter для завершения')
 
     elif n == '2':
-        # Восстанавливаем заметку из формата JSON
-        #reader = JsonNoteReader(file_name)
-        #read_notes = reader.read()
-        note = Note.Note("Заголовок заметки", "Текст заметки")
-        note.print_note()
-
-        enter = input('Нажмите Enter для выхода в меню ')
+        # Печатаем заметку из формата JSON
+        Note(file_name).print_note()
 
     elif n == '3':
-        #operations.refactor()
-        enter = input('Нажмите Enter для выхода в меню ')
+        Note(file_name).print_title()
+        search = input('Введите имя заголовка для поиска: ')
+        new_body = input('Напиши новый текст: ').strip()
+        Correct_body(file_name).correct_body(search, new_body, datetime.now())
 
     elif n == '4':
-        user_search = input('Введите имя для поиска?: ')
-        #searchcontact.searchcontact(user_search)
-        enter = input('Нажмите Enter для завершения')
+        Note(file_name).print_title()
+        user_search = input('Введите имя заголовка для поиска: ')
+        Search_title(file_name).find_body(user_search)
 
     elif n == '5':
-        #delete()
-        enter = input('Нажмите Enter для завершения')
+        Note(file_name).print_title()
+        delete_note = input('Введите имя заголовка для удаления: ')
+        Delete_note(file_name).delete_body(delete_note)
