fcf627f254c19de8653c61265fe1283e96ee050d
den-m1985 <denismartyanov244@gmail.com>
2023-04-08 19:28:18 +0200

    added print Note

--- Note.py
+++ Note.py
@@ -1,15 +1,25 @@
-import json
+from JsonNoteReader import JsonNoteReader
 
 
 class Note:
-    def __init__(self, title, body):
+    def __init__(self, file_name):
         self.id = None  # идентификатор
-        self.title = title  # заголовок
-        self.body = body  # тело заметки
         self.created_at = None  # дату/время создания
+        self.file_name = file_name
 
     def set_id(self, note_id):
         self.id = note_id
+
+    # заголовок
+    def set_title(self, title):
+        self.title = title
+
+    # тело заметки
+    def set_body(self, body):
+        self.body = body
+
+    def set_created_at(self, created_at):
+        self.created_at = created_at
 
     def get_id(self):
         return self.id
@@ -23,9 +33,6 @@ class Note:
     def get_created_at(self):
         return self.created_at
 
-    def set_created_at(self, created_at):
-        self.created_at = created_at
-
     def to_dict(self):
         note_dict = {}
         note_dict["id"] = self.id
@@ -35,48 +42,18 @@ class Note:
         return note_dict
 
     def print_note(self):
-        print(self.title)
-        print(self.body)
-        print(self.created_at)
+        reader = JsonNoteReader(self.file_name)
+        data = reader.read()
+        sorted_data = sorted(data, key=lambda item: item['created_at'])
+        for item in sorted_data:
+            print(f'--{item["title"]}--')
+            print(item["body"])
+            print(item["created_at"])
+            print()
 
-
-    @classmethod
-    def from_dict(cls, note_dict):
-        return cls(
-            note_dict["title"],
-            note_dict["body"],
-            note_dict["created_at"]
-        )
-
-    @classmethod
-    def read_notes(cls, filename):
-        notes = []
-        try:
-            with open(filename, "r", encoding="utf-8") as f:
-                notes_dict = json.load(f)
-                for note_dict in notes_dict:
-                    note = cls.from_dict(note_dict)
-                    notes.append(note)
-        except FileNotFoundError:
-            pass
-        return notes
-
-    @classmethod
-    def read_max_id(cls, filename):
-        try:
-            with open(filename, "r", encoding="utf-8") as f:
-                max_id = int(f.read())
-        except FileNotFoundError:
-            max_id = 0
-        return max_id
-
-    @classmethod
-    def write_max_id(cls, filename, max_id):
-        with open(filename, "w", encoding="utf-8") as f:
-            f.write(str(max_id))
-
-    @classmethod
-    def generate_id(cls, filename):
-        max_id = cls.read_max_id(filename) + 1
-        cls.write_max_id(filename, max_id)
-        return max_id
+    def print_title(self):
+        reader = JsonNoteReader(self.file_name)
+        data = reader.read()
+        print('Список заголовков:')
+        for item in data:
+            print(f'--{item["title"]}--')
